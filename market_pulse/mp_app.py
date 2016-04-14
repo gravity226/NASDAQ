25b01952e95ff77148545a4b722a19265557b8b8
Tommy Jay <gravity226@gmail.com>
2016-04-14 13:50:33 -0600

    Added sessions to the web app

--- market_pulse/mp_app.py
+++ market_pulse/mp_app.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import requests
 from top_companies import hand_made_list
+import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 
 from yahoo_finance import Share
 import pandas as pd
@@ -29,27 +31,43 @@ def get_data(sym):
 
 @app.route('/')
 def form():
+    if request.method == 'POST':
+        if request.form['submit'] == 'Submit':
+            session['active'] = False
     companies = hand_made_list()
     return render_template('form.html',
                             companies=companies.keys())
 
-@app.route('/display', methods=['POST'])
+@app.route('/display', methods=['POST', 'GET'])
 def display():
-    sym = str(request.form['user_input']).strip().upper() or 'SPY'
+    # Is the user coming from the form page?
+    try:
+        sym = str(request.form['user_input']).strip().upper() or 'SPY'
+        session['active'] = True
+        session['sym'] = sym
+    except:
+        # If a user goes to the display page and a session is not active
+        if session.get('active') != True:
+            sym = 'SPY'
+            session['active'] = True
+            session['sym'] = sym
+        else:
+            sym = session['sym'] # if a session is active leave the sym alone
+
     share = Share(sym)
     quote = float(share.get_price())
     com_name = hand_made_list()[sym][0]
 
     return render_template('display.html',
-                            sym=sym,
+                            sym=session['sym'],
                             com_name=com_name,
                             quote=quote)
 
 @app.route('/history')
 def history():
-    sym='AAPL'
+    sym = session['sym']
     share = Share(sym)
-    historical = share.get_historical('2016-03-13', '2016-04-12')
+    historical = share.get_historical('2016-03-13', '2016-04-13')
     canvas_list = []
     for day in historical:
         canvas_list.append([int(day['Date'][:4]),
@@ -75,7 +93,8 @@ def history():
                         ])
 
     return render_template('history.html',
-                            canvas_list=canvas_list)
+                            canvas_list=canvas_list,
+                            sym=sym)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
