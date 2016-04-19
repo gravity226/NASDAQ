e12e155f83581f71c0d6a499912beea10277e6e3
Tommy Jay <gravity226@gmail.com>
2016-04-19 11:46:39 -0600

    updating error handling for predict page

--- market_pulse/mp_app.py
+++ market_pulse/mp_app.py
@@ -44,8 +44,7 @@ def error():
         if request.form['submit'] == 'Submit':
             session['active'] = False
     companies = hand_made_list()
-    return render_template('error.html',
-                            companies=companies.keys())
+    return render_template('error.html')
 
 @app.route('/display', methods=['POST', 'GET'])
 def display():
@@ -76,38 +75,41 @@ def display():
         #                         sym=session['sym'])
     else:
         quote = float(share.get_price())
-        com_name = hand_made_list()[sym][0]
+        com_name = sym #hand_made_list()[sym][0]
 
-        historical = share.get_historical('2016-03-13', '2016-04-18')
-        canvas_list = []
-        for day in historical:
-            canvas_list.append([int(day['Date'][:4]),
-                                int(day['Date'][5:7]) - 1,
-                                int(day['Date'][-2:]),
-                                float(day['Open']),
-                                float(day['High']),
-                                float(day['Low']),
-                                float(day['Close'])
+        try:
+            historical = share.get_historical('2016-03-13', '2016-04-18')
+            canvas_list = []
+            for day in historical:
+                canvas_list.append([int(day['Date'][:4]),
+                                    int(day['Date'][5:7]) - 1,
+                                    int(day['Date'][-2:]),
+                                    float(day['Open']),
+                                    float(day['High']),
+                                    float(day['Low']),
+                                    float(day['Close'])
+                                    ])
+            info = share.get_info()
+            open = share.get_open()
+            high = share.get_days_high()
+            low = share.get_days_low()
+            price = share.get_price()
+            canvas_list.append([int(info['end'][:4]),
+                                int(info['end'][5:7]) - 1,
+                                int(info['end'][-2:]),
+                                float(open),
+                                float(high),
+                                float(low),
+                                float(price)
                                 ])
-        info = share.get_info()
-        open = share.get_open()
-        high = share.get_days_high()
-        low = share.get_days_low()
-        price = share.get_price()
-        canvas_list.append([int(info['end'][:4]),
-                            int(info['end'][5:7]) - 1,
-                            int(info['end'][-2:]),
-                            float(open),
-                            float(high),
-                            float(low),
-                            float(price)
-                            ])
 
-        return render_template('display.html',
-                                sym=session['sym'],
-                                com_name=com_name,
-                                quote=quote,
-                                canvas_list=canvas_list)
+            return render_template('display.html',
+                                    sym=session['sym'],
+                                    com_name=com_name,
+                                    quote=quote,
+                                    canvas_list=canvas_list)
+        except:
+            return redirect("/error")
 
 @app.route('/history')
 def history():
@@ -159,57 +161,60 @@ def predict():
     else:
         sym = session['sym'] # if a session is active leave the sym alone
 
-    share = Share(sym)
+    try:
+        share = Share(sym)
 
-    historical = share.get_historical('2016-04-12', '2016-04-18') # need to auto input the date...
+        historical = share.get_historical('2016-04-12', '2016-04-18') # need to auto input the date...
 
-    canvas_list = []
+        canvas_list = []
 
-    info = share.get_info()
-    open = share.get_open()
-    high = share.get_days_high()
-    low = share.get_days_low()
-    price = share.get_price()
-    canvas_list.append([int(info['end'][:4]),
-                        int(info['end'][5:7]) - 1,
-                        int(info['end'][-2:]),
-                        float(price)
-                        ])
-
-    for day in historical:
-        canvas_list.append([int(day['Date'][:4]),
-                            int(day['Date'][5:7]) - 1,
-                            int(day['Date'][-2:]),
-                            float(day['Close'])
+        info = share.get_info()
+        open = share.get_open()
+        high = share.get_days_high()
+        low = share.get_days_low()
+        price = share.get_price()
+        canvas_list.append([int(info['end'][:4]),
+                            int(info['end'][5:7]) - 1,
+                            int(info['end'][-2:]),
+                            float(price)
                             ])
 
-    df_preds = pd.read_csv('data/predictions.csv')
-    date = df_preds[df_preds['sym'] == sym]['date'].values[0]
-    historical_day = float(share.get_historical(date, date)[0]['Close'])
-    year = date[:4]
-    month = date[5:7]
-    day = date[-2:]
+        for day in historical:
+            canvas_list.append([int(day['Date'][:4]),
+                                int(day['Date'][5:7]) - 1,
+                                int(day['Date'][-2:]),
+                                float(day['Close'])
+                                ])
 
-    predicted_value = historical_day + df_preds[df_preds['sym'] == sym]['regressor_mse'].values[0]
-    predicted_rmse = df_preds[df_preds['sym'] == sym]['mse'].values[0] ** .5
+        df_preds = pd.read_csv('data/predictions.csv')
+        date = df_preds[df_preds['sym'] == sym]['date'].values[0]
+        historical_day = float(share.get_historical(date, date)[0]['Close'])
+        year = date[:4]
+        month = date[5:7]
+        day = date[-2:]
 
-    box1_low = predicted_value - (predicted_rmse / 2.)
-    box1_high = predicted_value + (predicted_rmse / 2.)
+        predicted_value = historical_day + df_preds[df_preds['sym'] == sym]['regressor_mse'].values[0]
+        predicted_rmse = df_preds[df_preds['sym'] == sym]['mse'].values[0] ** .5
 
-    box2_low = predicted_value - (predicted_rmse)
-    box2_high = predicted_value + (predicted_rmse)
+        box1_low = predicted_value - (predicted_rmse / 2.)
+        box1_high = predicted_value + (predicted_rmse / 2.)
 
-    return render_template('predict.html',
-                            canvas_list=canvas_list,
-                            sym=sym,
-                            box1_low=box1_low,
-                            box1_high=box1_high,
-                            box2_low = box2_low,
-                            box2_high=box2_high,
-                            year=year,
-                            month=month,
-                            day=day)
+        box2_low = predicted_value - (predicted_rmse)
+        box2_high = predicted_value + (predicted_rmse)
 
+        return render_template('predict.html',
+                                canvas_list=canvas_list,
+                                sym=sym,
+                                box1_low=box1_low,
+                                box1_high=box1_high,
+                                box2_low = box2_low,
+                                box2_high=box2_high,
+                                year=year,
+                                month=month,
+                                day=day)
+    except:
+        return redirect("/error")
+        
 @app.route('/test')
 def test():
     # Is the user coming from the form page?
